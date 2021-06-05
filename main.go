2597ea01f275c1e83e85b0966c1744a6d175186e
Hakan Kurtulus <kurtulus@mail.com>
2021-06-05 12:40:40 +0200

    OS-10 | Use cobra-commander (#3)
    * OS-10 | Use cobra-commander
    
    * fix lints

--- main.go
+++ main.go
@@ -3,14 +3,27 @@ package main
 import (
 	"os"
 
+	commander "github.com/omegion/cobra-commander"
 	"github.com/omegion/vault-unseal/cmd"
+	"github.com/spf13/cobra"
 )
 
 func main() {
-	commander := cmd.NewCommander()
-	commander.Setup()
+	root := &cobra.Command{
+		Use:          "vault-unseal",
+		Short:        "Vault Auto Unseal",
+		Long:         "CLI command to automatically unseal Vault",
+		SilenceUsage: true,
+	}
 
-	if err := commander.Root.Execute(); err != nil {
+	c := commander.NewCommander(root).
+		SetCommand(
+			cmd.Version(),
+			cmd.Unseal(),
+		).
+		Init()
+
+	if err := c.Execute(); err != nil {
 		os.Exit(1)
 	}
 }
